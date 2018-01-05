cfc693fa8cc74706b9cc91e1eb2ae1700f4d6827
Michael Dunn <michael.dunn@realself.com>
2018-01-04 18:01:59 -0800

    add gitignore, remove github download url

--- setup.py
+++ setup.py
@@ -2,10 +2,9 @@ from setuptools import setup
 
 setup(
 	name="pysinter",
-	version=1.0,
+	version=1.0.1,
 	description="Python 3 wrapper for the Sinter API",
 	url="https://github.com/rsmichaeldunn/pysinter",
-	download_url='https://github.com/rsmichaeldunn/pysinter/archive/1.0.tar.gz',
 	author="Michael Dunn",
 	author_email="michael.dunn@realself.com",
 	license="MIT",

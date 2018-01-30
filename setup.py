2d6bce21bce080b96f5f8b9a0efd6e9488b344ff
Michael Dunn <michael.dunn@realself.com>
2018-01-30 10:52:04 -0800

    Bump version for changes to license

--- setup.py
+++ setup.py
@@ -2,12 +2,13 @@ from setuptools import setup
 
 setup(
 	name="pysinter",
-	version="1.0.4",
+	version="1.0.5",
 	description="Python 3 wrapper for the Sinter API",
 	url="https://github.com/rsmichaeldunn/pysinter",
 	author="Michael Dunn",
 	author_email="michael.dunn@realself.com",
-	license="MIT",
+	classifiers=['Programming Language :: Python :: 3 :: Only'],
+	license="BSD-3-Clause",
 	install_requires=['requests'],
 	packages=['pysinter'],
 	keywords=['sinter', 'business intelligence', 'bi', 'etl', 'dbt']

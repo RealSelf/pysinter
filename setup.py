e455ace8de162be660e7f884e2997d32184a2048
Michael Dunn <michael.dunn@realself.com>
2018-02-22 12:18:19 -0800

    update repo URL

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
 	name="pysinter",
 	version="1.0.5",
 	description="Python 3 wrapper for the Sinter API",
-	url="https://github.com/rsmichaeldunn/pysinter",
+	url="https://github.com/RealSelf/pysinter",
 	author="Michael Dunn",
 	author_email="michael.dunn@realself.com",
 	classifiers=['Programming Language :: Python :: 3 :: Only'],

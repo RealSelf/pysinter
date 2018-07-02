ebcf1e149423431c995c057758838efad084f38c
Michael Dunn <michael.dunn@realself.com>
2018-07-02 10:10:32 -0700

    merge _get and _post into a single function to remove duplication
    move the account path to api_base since it's in every url endpoint

--- pysinter/pysinter.py
+++ pysinter/pysinter.py
@@ -15,48 +15,48 @@ class Sinter(object):
     * :py:meth:`trigger_job_run` - triggers an execution of a job definition, returns details of that job run
     """
 
-    def __init__(self, account_id, api_token):
+    def __init__(self, account_id, api_token, api_version=1):
         self.account_id = account_id
         self.api_token = api_token
-        self.api_base = 'https://app.sinterdata.com/api/v1'
+        self.api_version = api_version
+        self.api_base = 'https://app.sinterdata.com/api/v{}/accounts/{}'.format(self.api_version, self.account_id)
 
-    def _get(self, url_suffix):
+    def _do_request(self, method, url_suffix):
         url = self.api_base + url_suffix
         headers = {'Authorization': 'Token %s' % self.api_token}
-        response = requests.get(url, headers=headers)
-        if response.status_code == 200:
-            return json.loads(response.content)
-        else:
-            raise RuntimeError(response.content)
+        method = method.upper()
 
-    def _post(self, url_suffix):
-        url = self.api_base + url_suffix
-        headers = {'Authorization': 'token %s' % self.api_token}
-        response = requests.post(url, headers=headers)
-        if response.status_code == 201:
+        if method == "GET":
+            response = requests.get(url, headers=headers)
+        elif method == "POST":
+            response = requests.post(url, headers=headers)
+        else:
+            raise ValueError("method must equal either 'GET' or 'POST'")
+
+        if response.status_code == requests.codes.ok:
             return json.loads(response.content)
         else:
             raise RuntimeError(response.content)
 
     def list_projects(self):
-        return self._get('/accounts/%s/projects/' % self.account_id)['data']
+        return self._do_request('GET', '/projects/')['data']
 
     def get_project(self, project_id):
-        return self._get('/accounts/%s/projects/%s/' % (self.account_id, project_id))['data']
+        return self._do_request('GET', '/projects/%s/' % (project_id))['data']
 
     def list_job_definitions(self, project_id):
-        return self._get('/accounts/%s/projects/%s/definitions/' % (self.account_id, project_id))
+        return self._do_request('GET', '/projects/%s/definitions/' % (project_id))
 
     def get_job_definition(self, project_id, job_id):
         raise NotImplementedError
-        # return self._get('/accounts/%s/projects/%s/definitions/%s/' % (self.account_id, project_id, job_id))
+        # return self._do_request('/projects/%s/definitions/%s/' % (project_id, job_id))
 
     def list_job_runs(self, project_id, job_id):
         raise NotImplementedError
-        # return self._get('/accounts/%s/projects/%s/definitions/%s/runs/' % (self.account_id, project_id, job_id))
+        # return self._do_request('/projects/%s/definitions/%s/runs/' % (project_id, job_id))
 
     def get_job_run(self, project_id, job_run_id):
-        return self._get('/accounts/%s/projects/%s/runs/%s/' % (self.account_id, project_id, job_run_id))
+        return self._do_request('GET', '/projects/%s/runs/%s/' % (project_id, job_run_id))
 
     def trigger_job_run(self, project_id, job_id):
-        return self._post('/accounts/%s/projects/%s/definitions/%s/runs/' % (self.account_id, project_id, job_id))
+        return self._do_request('POST', '/projects/%s/definitions/%s/runs/' % (project_id, job_id))
